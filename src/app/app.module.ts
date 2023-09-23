708ad8de8a5817242ccb645c5ec705b68fa985a6
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2023-09-23 11:40:46 -0400

    Added angular version of MUI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatSlideToggleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
