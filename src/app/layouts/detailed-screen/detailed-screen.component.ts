6bfa6d5ed391b7b5ba51c4f71209a1b3592f22d6
Sigfredo Guzman <sigfredo.guzman@gmail.com>
2023-11-05 09:16:49 -0500

    Copy pasted more into recipes.json. Fixed CSS as well.

---
+++ src/app/layouts/detailed-screen/detailed-screen.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core'
+import { Ingredient, Recipe } from 'src/app/models/recipe.model'
+
+@Component({
+  selector: 'app-detailed-screen',
+  templateUrl: './detailed-screen.component.html',
+  styleUrls: ['./detailed-screen.component.css'],
+})
+export class DetailedScreenComponent {
+  @Input() ingredients: Ingredient[]
+  // @Input() recipe: Recipe
+
+  completedItems: string[] = []
+
+  toggleCompleted(ingredient: string): void {
+    if (this.completedItems.includes(ingredient)) {
+      this.completedItems = this.completedItems.filter((item) => item !== ingredient)
+    } else {
+      this.completedItems.push(ingredient)
+    }
+  }
+}
